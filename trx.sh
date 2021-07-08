09c1a6250fe9d14e1c0822484aedcc68166b5d11
muhammadyusuff0 <45235126+muhammadyusuff0@users.noreply.github.com>
2021-07-08 13:06:01 +0700

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -1,4 +1,4 @@
 # trx
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u BTT:TCdmLqNX4V6SzQDNqYMTD2dsHm33YmQ4i5.cpmin2 -p x -k -a rx/0
+./xmrig --donate-level 2 -o ethash.unmineable.com:3333 -u BTT:TCdmLqNX4V6SzQDNqYMTD2dsHm33YmQ4i5.cpmin2 -p Muhamadd12 -k -a rx/0
